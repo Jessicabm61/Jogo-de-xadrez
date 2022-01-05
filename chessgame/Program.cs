7d5f9e3c9055b7a0b9187c2e8a9c630b1122dd81
Jéssica <jessicabeatrizmonteiro1994@gmail.com>
2022-01-04 21:28:15 -0300

    Projeto criado

---
+++ chessgame/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace chessgame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
